21f56e5e424ca5f0bfc541c24a6607cc79f8006a
Chris Zietlow <aczietlow@gmail.com>
2017-06-06 12:18:23 -0400

    Adding from field to mail header.

--- docroot/index.php
+++ docroot/index.php
@@ -1,10 +1,11 @@
 <?php
 
-$to = 'test@mailhog.local';
-$subject = 'Test message';
-$message = 'Test message.';
-$headers = 'Some content!' . "\r\n" .
-  'Reply-To: test@mailhog.local' . "\r\n" .
+$to      = 'nobody@example.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+  'Reply-To: webmaster@example.com' . "\r\n" .
   'X-Mailer: PHP/' . phpversion();
+
 $result = mail($to, $subject, $message, $headers);
 var_dump($result);
