81efd1d14373836ba5cc2c3755ec710d21542c07
Chris Zietlow <aczietlow@gmail.com>
2017-06-06 11:06:40 -0400

    base configuration

--- docroot/index.php
+++ docroot/index.php
@@ -1,1 +1,10 @@
-<?php phpinfo();
+<?php
+
+$to = 'test@mailhog.local';
+$subject = 'Test message';
+$message = 'Test message.';
+$headers = 'Some content!' . "\r\n" .
+  'Reply-To: test@mailhog.local' . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+$result = mail($to, $subject, $message, $headers);
+var_dump($result);
